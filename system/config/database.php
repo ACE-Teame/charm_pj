0b475030bb59ecf2d4db82cc54972841f7ec382a
mingzhongshui <878039245@qq.com>
2017-07-14 15:15:19 +0800

    database

--- system/config/database.php
+++ system/config/database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 return [
@@ -8,15 +7,5 @@ return [
 	'username'      => 'root',
 	'password'      => '123456',
 	'charset'       => 'utf8',
-=======
-<?php 
 
-return [
-	'database_type' => 'mysql',
-	'database_name' => 'charm_pj',
-	'server'        => 'localhost',
-	'username'      => 'root',
-	'password'      => 'root-2017',
-	'charset'       => 'utf8',
->>>>>>> 347a38e0ba51fd8727122623e5e2b90300e85a3e
 ];
