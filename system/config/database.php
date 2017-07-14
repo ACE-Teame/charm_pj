df380fe760ae4ac2f80dbb7629d01a946c85c7b4
Iris88zjm <327055610@qq.com>
2017-07-14 14:33:20 +0800

    update gitignore

---
+++ system/config/database.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+	'database_type' => 'mysql',
+	'database_name' => 'charm_pj',
+	'server'        => 'localhost',
+	'username'      => 'root',
+	'password'      => '',
+	'charset'       => 'utf8',
+];
