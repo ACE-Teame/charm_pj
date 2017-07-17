326fb4d5440b3a272aeee970b5039bd4a1208b98
mingzhongshui <878039245@qq.com>
2017-07-17 10:27:48 +0800

    edit log save path

--- system/drivers/log/file.php
+++ system/drivers/log/file.php
@@ -19,8 +19,6 @@ class file
 		$strSaveFileName = $pathSaveFloder . $file . '.log';
 		$jsonSaveContent = date('Y-m-d H:i:s') . '   ' . json_encode($data);
 		if(is_dir($pathSaveFloder)) {
-			// dump($pathSaveFloder);
-			// dump($strSaveFileName);exit;
 			file_put_contents($strSaveFileName, $jsonSaveContent . PHP_EOL, FILE_APPEND);
 		}else {
 			mkdir($pathSaveFloder, 0755, true);
