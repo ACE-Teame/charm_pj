52e691935ea889b9560667abecf42e45fc3db0da
Iris88zjm <327055610@qq.com>
2017-07-20 16:17:42 +0800

    add favicon.ico

--- app/views/header.php
+++ app/views/header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
 	<title>链接管理系统</title>
 	<link rel="stylesheet" type="text/css" href="//at.alicdn.com/t/font_yjtucfw03b8uayvi.css">
+	<link rel="icon" href="<?php echo base_url('resource/images/favicon.ico') ?>" sizes="192x192">
 	<?php echo css('main.css');?>
 </head>
 <body>
