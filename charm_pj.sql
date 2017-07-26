40707384f8518251108a911502611619d721e0d9
yifan <878039245@qq.com>
2017-07-26 21:40:04 +0800

    edit link content

--- charm_pj.sql
+++ charm_pj.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50505
-Source Host           : localhost:3306
+Source Server         : 118.190.172.204
+Source Server Version : 50718
+Source Host           : 118.190.172.204:3306
 Source Database       : charm_pj
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2017-07-25 22:59:42
+Date: 2017-07-26 21:39:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -40,7 +40,7 @@ INSERT INTO `address` VALUES ('10', '深圳', '1500262136', '1500262136', '1', '
 INSERT INTO `address` VALUES ('11', '厦门', '1500262392', '1500262392', '1', '1');
 INSERT INTO `address` VALUES ('12', '天津', '1500262399', '1500262399', '1', '1');
 INSERT INTO `address` VALUES ('13', '杭州', '1500262413', '1500262413', '1', '1');
-INSERT INTO `address` VALUES ('14', '湖南', '1500262435', '1500262435', '1', '1');
+INSERT INTO `address` VALUES ('14', '重庆', '1500262435', '1501041487', '1', '1');
 
 -- ----------------------------
 -- Table structure for `domain`
@@ -80,10 +80,10 @@ CREATE TABLE `group` (
 -- ----------------------------
 -- Records of group
 -- ----------------------------
-INSERT INTO `group` VALUES ('2', '管理员', '管理员啊2', '1499949313', '1');
-INSERT INTO `group` VALUES ('4', '测试组哈哈', '测试组哈哈', '1500163308', '1');
-INSERT INTO `group` VALUES ('5', '超管组', '超管组1', '1500163331', '1');
-INSERT INTO `group` VALUES ('6', '王大锤', '哈哈哈哈', '1500163618', '1');
+INSERT INTO `group` VALUES ('2', '管理员', '超级管理员', '1499949313', '1');
+INSERT INTO `group` VALUES ('4', '优化经理', '优化经理', '1500163308', '1');
+INSERT INTO `group` VALUES ('5', '优化师', '优化师', '1500163331', '1');
+INSERT INTO `group` VALUES ('6', '销售', '销售', '1500163618', '1');
 
 -- ----------------------------
 -- Table structure for `link`
@@ -107,9 +107,9 @@ CREATE TABLE `link` (
 -- ----------------------------
 -- Records of link
 -- ----------------------------
-INSERT INTO `link` VALUES ('2', '1', '1', '1.hp', '3s.php', '2.php', '1', '1500470227', '1', '1500559829', '0');
+INSERT INTO `link` VALUES ('2', '3', '1', '1.php', '3s.php', '2.php', '1', '1500470227', '1', '1501047985', '0');
 INSERT INTO `link` VALUES ('3', '2', '1', '1.hp', '3.php', '2t.php', '1', '1500470320', '1', '1500994326', '0');
-INSERT INTO `link` VALUES ('4', '1', '1', '1.php', '3.php', '2.php', '1', '1500470387', '1', '1500557989', '0');
+INSERT INTO `link` VALUES ('4', '2', '1', '1.php', '3.php', '2.php', '1', '1500470387', '1', '1501047993', '0');
 INSERT INTO `link` VALUES ('5', '2', '1', 'test.php', 'test.php', 'test.php', '1', '1500556009', '1', '1500559216', '0');
 INSERT INTO `link` VALUES ('6', '4', '2', 'souhu.php', 'souhutuiguang.php', 'souhushenhe.php', '1', '1500992201', '1', '1500994339', '0');
 
@@ -125,19 +125,20 @@ CREATE TABLE `link_address` (
 -- ----------------------------
 -- Records of link_address
 -- ----------------------------
+INSERT INTO `link_address` VALUES ('5', '5');
+INSERT INTO `link_address` VALUES ('5', '8');
+INSERT INTO `link_address` VALUES ('5', '10');
+INSERT INTO `link_address` VALUES ('5', '12');
+INSERT INTO `link_address` VALUES ('3', '8');
+INSERT INTO `link_address` VALUES ('6', '10');
+INSERT INTO `link_address` VALUES ('6', '12');
+INSERT INTO `link_address` VALUES ('2', '5');
+INSERT INTO `link_address` VALUES ('2', '6');
 INSERT INTO `link_address` VALUES ('4', '5');
 INSERT INTO `link_address` VALUES ('4', '6');
 INSERT INTO `link_address` VALUES ('4', '10');
 INSERT INTO `link_address` VALUES ('4', '12');
 INSERT INTO `link_address` VALUES ('4', '14');
-INSERT INTO `link_address` VALUES ('5', '5');
-INSERT INTO `link_address` VALUES ('5', '8');
-INSERT INTO `link_address` VALUES ('5', '10');
-INSERT INTO `link_address` VALUES ('5', '12');
-INSERT INTO `link_address` VALUES ('2', '9');
-INSERT INTO `link_address` VALUES ('3', '8');
-INSERT INTO `link_address` VALUES ('6', '10');
-INSERT INTO `link_address` VALUES ('6', '12');
 
 -- ----------------------------
 -- Table structure for `link_content`
@@ -147,17 +148,20 @@ CREATE TABLE `link_content` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `link_id` int(11) NOT NULL COMMENT '链接ID',
   `title` varchar(256) DEFAULT NULL COMMENT '标题',
+  `display_page` tinyint(1) NOT NULL DEFAULT '0',
   `price` float(10,2) DEFAULT NULL COMMENT '价格',
-  `buy_num` int(11) DEFAULT NULL COMMENT '购买数量',
-  `discount` float(4,2) DEFAULT NULL COMMENT '折扣',
+  `buy_count` int(11) DEFAULT NULL COMMENT '购买数量',
+  `discount` float(4,1) DEFAULT NULL COMMENT '折扣',
   `end_time` varchar(36) DEFAULT NULL COMMENT '结束时间',
-  `campany` varchar(64) DEFAULT NULL COMMENT '公司名称',
+  `company_name` varchar(64) DEFAULT NULL COMMENT '公司名称',
   `icp` varchar(64) DEFAULT NULL,
-  `mobile` varchar(24) DEFAULT NULL COMMENT '电话',
+  `phone` varchar(24) DEFAULT NULL COMMENT '电话',
   `wechat` varchar(36) DEFAULT NULL COMMENT '微信号',
-  `product_photo` varchar(128) DEFAULT NULL COMMENT '产品主图',
-  `buy_flow` text COMMENT '购买流程',
-  `synopsis` text COMMENT '简介',
+  `main_image` varchar(128) DEFAULT NULL COMMENT '产品主图',
+  `process` text COMMENT '购买流程',
+  `description` text COMMENT '简介',
+  `reply` text COMMENT '客户留言',
+  `is_show_time` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否显示时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='链接内容';
 
@@ -177,14 +181,18 @@ CREATE TABLE `section` (
   `last_edit_uid` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of section
 -- ----------------------------
-INSERT INTO `section` VALUES ('1', '测试部', '1499783827', '1500209545', '1', '1');
-INSERT INTO `section` VALUES ('2', '网络部', '1499783914', '1500209624', '1', '1');
-INSERT INTO `section` VALUES ('4', '人事部', '1500209712', '1500209877', '1', '1');
+INSERT INTO `section` VALUES ('1', '管理员', '1499783827', '1501041589', '1', '1');
+INSERT INTO `section` VALUES ('2', '扶翼', '1499783914', '1501041419', '1', '1');
+INSERT INTO `section` VALUES ('4', '陌陌', '1500209712', '1501041430', '1', '1');
+INSERT INTO `section` VALUES ('5', '电商', '1501041440', '1501041440', '1', '1');
+INSERT INTO `section` VALUES ('6', 'TSA', '1501041446', '1501041446', '1', '1');
+INSERT INTO `section` VALUES ('7', 'KA', '1501041450', '1501041450', '1', '1');
+INSERT INTO `section` VALUES ('8', '微博', '1501041596', '1501041596', '1', '1');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -202,13 +210,14 @@ CREATE TABLE `user` (
   `ip` varchar(48) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '2', '2', '1500190776', '1500304057', '1500989785', '127.0.0.1', '1');
-INSERT INTO `user` VALUES ('2', '测试', '$2y$10$AaMn.P5IufgJWqPFHyMyGOFnpnsbYX.pNL1NQqdgZXOFY65hPx1QG', '2', '1', '1500190931', '1500202634', '1500385198', null, '1');
-INSERT INTO `user` VALUES ('3', 'local', '$2y$10$xYiRivRPy8sZT210IgGTEeWTYmP4VTqzn2Fc5Grn48qI6e7CibWdi', '3', '2', '1500190971', '1500190971', null, null, '1');
-INSERT INTO `user` VALUES ('4', '三胖', '$2y$10$eVNy0sdyw7DHCsy5f2meYen1S4nsWZVnvfZzVzNYMmQpw.5UVCjia', '2', '2', '1500191438', '1500210382', null, null, '1');
-INSERT INTO `user` VALUES ('6', 'admin', '$2y$10$rdM2Z9t8hXv7qZ8K0WYmm.La8j7FfxOmq9wXQXtjcB4atwe/8gUny', '3', '2', '1500259529', '1500259529', null, null, '1');
+INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '1', '2', '1500190776', '1501041620', '1501047847', '113.91.161.178', '1');
+INSERT INTO `user` VALUES ('2', '张总', '$2y$10$irrwAcryVSSzgsLYcabPOeF/4Y6DEjy/QuvYsux9JPa4Q3vllOeQC', '2', '4', '1500190931', '1501047879', '1500385198', null, '1');
+INSERT INTO `user` VALUES ('3', '小刘', '$2y$10$VQIJ2MdM3KnvlEYOC5ZWYOlw9vWBiS29TF.B86F7sUlrcs7jP67pm', '2', '5', '1500190971', '1501047909', null, null, '1');
+INSERT INTO `user` VALUES ('4', '小夏', '$2y$10$SaAM883JuAFpia5LFI51V.3lYcs5LPvccjU1LDFBD5QaXkXBqLDJy', '2', '6', '1500191438', '1501047944', null, null, '1');
+INSERT INTO `user` VALUES ('6', 'test', '$2y$10$J6/CuDghuICeXjHOpJey2e0SRW40JpsBGbNj77Fg4DVQbQaTfGmu.', '2', '2', '1500259529', '1501041787', '1501039780', '113.91.161.178', '1');
+INSERT INTO `user` VALUES ('7', '小周', '$2y$10$YJJ5TsHES6ecO0EeY5C9/eiehXSkDrZvjVewAB74MV.CN8MjopAJS', '2', '5', '1501048187', '1501048187', null, null, '1');
