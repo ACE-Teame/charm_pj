9b08510b8fca0c67adb75aa24c8426c1c80ada9a
yifan <878039245@qq.com>
2017-08-02 22:11:10 +0800

    update mysql file

--- charm_pj.sql
+++ charm_pj.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-07-27 23:31:32
+Date: 2017-08-02 22:10:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -86,6 +86,32 @@ INSERT INTO `group` VALUES ('5', '优化师', '优化师', '1500163331', '1');
 INSERT INTO `group` VALUES ('6', '销售', '销售', '1500163618', '1');
 
 -- ----------------------------
+-- Table structure for `groupmenu`
+-- ----------------------------
+DROP TABLE IF EXISTS `groupmenu`;
+CREATE TABLE `groupmenu` (
+  `group_id` int(11) NOT NULL COMMENT '权限ID',
+  `menu_id` int(11) NOT NULL COMMENT '菜单ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of groupmenu
+-- ----------------------------
+INSERT INTO `groupmenu` VALUES ('2', '6');
+INSERT INTO `groupmenu` VALUES ('2', '7');
+INSERT INTO `groupmenu` VALUES ('2', '8');
+INSERT INTO `groupmenu` VALUES ('2', '9');
+INSERT INTO `groupmenu` VALUES ('2', '10');
+INSERT INTO `groupmenu` VALUES ('2', '11');
+INSERT INTO `groupmenu` VALUES ('2', '12');
+INSERT INTO `groupmenu` VALUES ('4', '8');
+INSERT INTO `groupmenu` VALUES ('4', '9');
+INSERT INTO `groupmenu` VALUES ('5', '8');
+INSERT INTO `groupmenu` VALUES ('5', '9');
+INSERT INTO `groupmenu` VALUES ('6', '8');
+INSERT INTO `groupmenu` VALUES ('6', '9');
+
+-- ----------------------------
 -- Table structure for `link`
 -- ----------------------------
 DROP TABLE IF EXISTS `link`;
@@ -157,7 +183,7 @@ CREATE TABLE `link_content` (
   `icp` varchar(64) DEFAULT NULL,
   `phone` varchar(24) DEFAULT NULL COMMENT '电话',
   `wechat` varchar(36) DEFAULT NULL COMMENT '微信号',
-  `main_image` varchar(128) DEFAULT NULL COMMENT '产品主图',
+  `main_image` text COMMENT '产品主图',
   `process` text COMMENT '购买流程',
   `description` text COMMENT '简介',
   `reply` text COMMENT '客户留言',
@@ -165,13 +191,41 @@ CREATE TABLE `link_content` (
   `create_time` int(10) NOT NULL,
   `update_time` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='链接内容';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='链接内容';
 
 -- ----------------------------
 -- Records of link_content
 -- ----------------------------
-INSERT INTO `link_content` VALUES ('1', '2', '测试', '0', '174.50', '156', '6.7', '2017-06-11 20:41:36', '福田', 'icp123123', '123123123', 'wechat', '&lt;p&gt;&lt;img src=&quot;ueditorphpuploadimage201707271501164705.jpg&quot; title=&quot;1501164705.jpg&quot; alt=&quot;63634789', '&lt;p&gt;1、先经过什么&lt;p&gt;&lt;p&gt;2、再经过什么&lt;p&gt;', '&lt;p&gt;很好用啊这个&lt;p&gt;', '&lt;p&gt;很好啊&lt;p&gt;', '0', '1501165179', '1501165179');
-INSERT INTO `link_content` VALUES ('2', '2', '测试数据展示', '0', '98.70', '167', '8.9', '2017-06-11 20:41:36', '福田自定义公司', 'icp878728372837', '18897283723', 'wechat', '&lt;p style=&quot;text-align:center&quot;&gt;&lt;img src=&quot;ueditorphpuploadimage201707271501165497.jpg&quot; title=&quot;150', '&lt;p&gt;1、先给我看一下&lt;p&gt;&lt;p&gt;2、再给别人看一下&lt;p&gt;&lt;p&gt;&lt;br&gt;&lt;p&gt;', '&lt;p&gt;白色、柱状&lt;p&gt;', '&lt;p&gt;很好用啊&lt;p&gt;', '0', '1501165533', '1501165533');
+INSERT INTO `link_content` VALUES ('1', '2', '测试', '0', '174.50', '156', '6.7', '2017-06-11 20:41:36', '福田2', 'icp123123', '123123123', 'wechat', '&lt;p&gt;&lt;img src=&quot;/ueditor/php/upload/image/20170728/1501246866.jpg&quot; title=&quot;1501246866.jpg&quot; alt=&quot;6363478936865030373830353.jpg&quot;/&gt;&lt;/p&gt;&lt;p&gt;&lt;img src=&quot;./ueditor/php/upload/image/20170728/1501248671.jpg&quot; title=&quot;1501248671.jpg&quot; alt=&quot;2uucyj1vlhyvjjr2779.jpg&quot;/&gt;&lt;/p&gt;', '&lt;p&gt;1、先经过什么&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;2、再经过什么&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;3、好了些吗&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '&lt;p&gt;很好用啊这个&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '&lt;p&gt;很好啊&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '0', '1501165179', '1501248866');
+
+-- ----------------------------
+-- Table structure for `menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(48) NOT NULL,
+  `url` varchar(64) DEFAULT NULL,
+  `pid` tinyint(1) NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+INSERT INTO `menu` VALUES ('1', '账号管理', '', '0', '1');
+INSERT INTO `menu` VALUES ('2', '链接管理', null, '0', '1');
+INSERT INTO `menu` VALUES ('3', '地区管理', null, '0', '1');
+INSERT INTO `menu` VALUES ('4', '部门管理', null, '0', '1');
+INSERT INTO `menu` VALUES ('5', '域名管理', null, '0', '1');
+INSERT INTO `menu` VALUES ('6', '权限管理', 'group/index', '1', '1');
+INSERT INTO `menu` VALUES ('7', '用户管理', 'user/index', '1', '1');
+INSERT INTO `menu` VALUES ('8', '链接跳转', 'link/skip', '2', '1');
+INSERT INTO `menu` VALUES ('9', '链接内容', 'link/link', '2', '1');
+INSERT INTO `menu` VALUES ('10', '屏蔽地区', 'address/index', '3', '1');
+INSERT INTO `menu` VALUES ('11', '部门', 'section/index', '4', '1');
+INSERT INTO `menu` VALUES ('12', '域名', 'domain/index', '5', '1');
 
 -- ----------------------------
 -- Table structure for `section`
@@ -219,9 +273,9 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '1', '2', '1500190776', '1501041620', '1501163111', '127.0.0.1', '1');
-INSERT INTO `user` VALUES ('2', '张总', '$2y$10$irrwAcryVSSzgsLYcabPOeF/4Y6DEjy/QuvYsux9JPa4Q3vllOeQC', '2', '4', '1500190931', '1501047879', '1500385198', null, '1');
-INSERT INTO `user` VALUES ('3', '小刘', '$2y$10$VQIJ2MdM3KnvlEYOC5ZWYOlw9vWBiS29TF.B86F7sUlrcs7jP67pm', '2', '5', '1500190971', '1501047909', null, null, '1');
-INSERT INTO `user` VALUES ('4', '小夏', '$2y$10$SaAM883JuAFpia5LFI51V.3lYcs5LPvccjU1LDFBD5QaXkXBqLDJy', '2', '6', '1500191438', '1501047944', null, null, '1');
+INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '1', '2', '1500190776', '1501041620', '1501682281', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('2', '张总', '$2y$10$irrwAcryVSSzgsLYcabPOeF/4Y6DEjy/QuvYsux9JPa4Q3vllOeQC', '2', '4', '1500190931', '1501047879', '1501682266', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('3', '小刘', '$2y$10$VQIJ2MdM3KnvlEYOC5ZWYOlw9vWBiS29TF.B86F7sUlrcs7jP67pm', '2', '5', '1500190971', '1501047909', '1501682302', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('4', '小夏', '$2y$10$SaAM883JuAFpia5LFI51V.3lYcs5LPvccjU1LDFBD5QaXkXBqLDJy', '2', '6', '1500191438', '1501047944', '1501682311', '127.0.0.1', '1');
 INSERT INTO `user` VALUES ('6', 'test', '$2y$10$J6/CuDghuICeXjHOpJey2e0SRW40JpsBGbNj77Fg4DVQbQaTfGmu.', '2', '2', '1500259529', '1501041787', '1501039780', '113.91.161.178', '1');
 INSERT INTO `user` VALUES ('7', '小周', '$2y$10$YJJ5TsHES6ecO0EeY5C9/eiehXSkDrZvjVewAB74MV.CN8MjopAJS', '2', '5', '1501048187', '1501048187', null, null, '1');
