048c5b4d40ccf866f3ab4fa3588562cf32786092
yifan <878039245@qq.com>
2017-08-08 21:58:29 +0800

    update sql

--- charm_pj.sql
+++ charm_pj.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-08-02 22:10:22
+Date: 2017-08-08 21:57:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -53,7 +53,7 @@ CREATE TABLE `domain` (
   `create_uid` int(11) NOT NULL COMMENT '创建者ID',
   `create_time` int(10) NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='域名管理';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='域名管理';
 
 -- ----------------------------
 -- Records of domain
@@ -63,6 +63,7 @@ INSERT INTO `domain` VALUES ('2', '搜狐', 'www.sohu.com', '1', '1500467666');
 INSERT INTO `domain` VALUES ('4', '雅虎', 'yahuu.com', '1', '1500564885');
 INSERT INTO `domain` VALUES ('5', '新浪', 'sina.com', '1', '1500565846');
 INSERT INTO `domain` VALUES ('6', '命中水', 'cxiansheng.cn', '1', '1500565869');
+INSERT INTO `domain` VALUES ('7', 'charm测试', 'charmpjtest.com', '1', '1502195646');
 
 -- ----------------------------
 -- Table structure for `group`
@@ -126,18 +127,19 @@ CREATE TABLE `link` (
   `creat_time` int(10) NOT NULL COMMENT '创建时间',
   `last_edit_uid` int(11) NOT NULL,
   `last_edit_time` int(10) NOT NULL,
-  `page_view` int(11) NOT NULL DEFAULT '0',
+  `is_pass` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='链接跳转表';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='链接跳转表';
 
 -- ----------------------------
 -- Records of link
 -- ----------------------------
-INSERT INTO `link` VALUES ('2', '3', '1', '1.php', '3s.php', '2.php', '1', '1500470227', '1', '1501047985', '0');
+INSERT INTO `link` VALUES ('2', '1', '1', '1.php', '3s.php', '2.php', '1', '1500470227', '1', '1502197824', '0');
 INSERT INTO `link` VALUES ('3', '2', '1', '1.hp', '3.php', '2t.php', '1', '1500470320', '1', '1500994326', '0');
 INSERT INTO `link` VALUES ('4', '2', '1', '1.php', '3.php', '2.php', '1', '1500470387', '1', '1501047993', '0');
 INSERT INTO `link` VALUES ('5', '2', '1', 'test.php', 'test.php', 'test.php', '1', '1500556009', '1', '1500559216', '0');
 INSERT INTO `link` VALUES ('6', '4', '2', 'souhu.php', 'souhutuiguang.php', 'souhushenhe.php', '1', '1500992201', '1', '1500994339', '0');
+INSERT INTO `link` VALUES ('7', '1', '7', 'origin', 'baidu.com', 'approl', '1', '1502195711', '1', '1502197656', '1');
 
 -- ----------------------------
 -- Table structure for `link_address`
@@ -158,13 +160,16 @@ INSERT INTO `link_address` VALUES ('5', '12');
 INSERT INTO `link_address` VALUES ('3', '8');
 INSERT INTO `link_address` VALUES ('6', '10');
 INSERT INTO `link_address` VALUES ('6', '12');
-INSERT INTO `link_address` VALUES ('2', '5');
-INSERT INTO `link_address` VALUES ('2', '6');
 INSERT INTO `link_address` VALUES ('4', '5');
 INSERT INTO `link_address` VALUES ('4', '6');
 INSERT INTO `link_address` VALUES ('4', '10');
 INSERT INTO `link_address` VALUES ('4', '12');
 INSERT INTO `link_address` VALUES ('4', '14');
+INSERT INTO `link_address` VALUES ('7', '8');
+INSERT INTO `link_address` VALUES ('7', '10');
+INSERT INTO `link_address` VALUES ('7', '11');
+INSERT INTO `link_address` VALUES ('2', '5');
+INSERT INTO `link_address` VALUES ('2', '6');
 
 -- ----------------------------
 -- Table structure for `link_content`
@@ -191,12 +196,13 @@ CREATE TABLE `link_content` (
   `create_time` int(10) NOT NULL,
   `update_time` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='链接内容';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='链接内容';
 
 -- ----------------------------
 -- Records of link_content
 -- ----------------------------
 INSERT INTO `link_content` VALUES ('1', '2', '测试', '0', '174.50', '156', '6.7', '2017-06-11 20:41:36', '福田2', 'icp123123', '123123123', 'wechat', '&lt;p&gt;&lt;img src=&quot;/ueditor/php/upload/image/20170728/1501246866.jpg&quot; title=&quot;1501246866.jpg&quot; alt=&quot;6363478936865030373830353.jpg&quot;/&gt;&lt;/p&gt;&lt;p&gt;&lt;img src=&quot;./ueditor/php/upload/image/20170728/1501248671.jpg&quot; title=&quot;1501248671.jpg&quot; alt=&quot;2uucyj1vlhyvjjr2779.jpg&quot;/&gt;&lt;/p&gt;', '&lt;p&gt;1、先经过什么&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;2、再经过什么&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;3、好了些吗&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '&lt;p&gt;很好用啊这个&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '&lt;p&gt;很好啊&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '0', '1501165179', '1501248866');
+INSERT INTO `link_content` VALUES ('2', '7', 'charm测试页面', '0', '98.90', '1000', '0.9', '2017-10-11 20:41:36', 'charm公司', 'charm0987723', '8786-2837822', 'wechat-charm', '&lt;p style=&quot;text-align:center&quot;&gt;&lt;img src=&quot;/resource/images/20170808/1502195806.jpg&quot; title=&quot;1502195806.jpg&quot; alt=&quot;6363478936865030373830353.jpg&quot; width=&quot;632&quot; height=&quot;729&quot;/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;', '&lt;p&gt;1.这个&amp;nbsp;&lt;/p&gt;', '&lt;p&gt;好用&lt;/p&gt;', '&lt;p&gt;1&lt;/p&gt;', '1', '1502195843', '1502195843');
 
 -- ----------------------------
 -- Table structure for `menu`
@@ -273,9 +279,9 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '1', '2', '1500190776', '1501041620', '1501682281', '127.0.0.1', '1');
-INSERT INTO `user` VALUES ('2', '张总', '$2y$10$irrwAcryVSSzgsLYcabPOeF/4Y6DEjy/QuvYsux9JPa4Q3vllOeQC', '2', '4', '1500190931', '1501047879', '1501682266', '127.0.0.1', '1');
-INSERT INTO `user` VALUES ('3', '小刘', '$2y$10$VQIJ2MdM3KnvlEYOC5ZWYOlw9vWBiS29TF.B86F7sUlrcs7jP67pm', '2', '5', '1500190971', '1501047909', '1501682302', '127.0.0.1', '1');
-INSERT INTO `user` VALUES ('4', '小夏', '$2y$10$SaAM883JuAFpia5LFI51V.3lYcs5LPvccjU1LDFBD5QaXkXBqLDJy', '2', '6', '1500191438', '1501047944', '1501682311', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('1', 'admin', '$2y$10$Yhis6.Ph76n1StRMFv/cqeOlwyFRwmpTKLCP0yhdMYvy.wDErcapm', '1', '2', '1500190776', '1501041620', '1502195498', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('2', '张总', '$2y$10$irrwAcryVSSzgsLYcabPOeF/4Y6DEjy/QuvYsux9JPa4Q3vllOeQC', '2', '4', '1500190931', '1501047879', '1501769047', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('3', '小刘', '$2y$10$VQIJ2MdM3KnvlEYOC5ZWYOlw9vWBiS29TF.B86F7sUlrcs7jP67pm', '2', '5', '1500190971', '1501047909', '1501768162', '127.0.0.1', '1');
+INSERT INTO `user` VALUES ('4', '小夏', '$2y$10$SaAM883JuAFpia5LFI51V.3lYcs5LPvccjU1LDFBD5QaXkXBqLDJy', '2', '6', '1500191438', '1501047944', '1501686782', '127.0.0.1', '1');
 INSERT INTO `user` VALUES ('6', 'test', '$2y$10$J6/CuDghuICeXjHOpJey2e0SRW40JpsBGbNj77Fg4DVQbQaTfGmu.', '2', '2', '1500259529', '1501041787', '1501039780', '113.91.161.178', '1');
 INSERT INTO `user` VALUES ('7', '小周', '$2y$10$YJJ5TsHES6ecO0EeY5C9/eiehXSkDrZvjVewAB74MV.CN8MjopAJS', '2', '5', '1501048187', '1501048187', null, null, '1');
