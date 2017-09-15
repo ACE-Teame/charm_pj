7fa0e56862bbe6a536b6d98ef86c8b5fb6ad4320
Iris88zjm <327055610@qq.com>
2017-09-15 11:47:03 +0800

    modify database password

--- system/config/database.php
+++ system/config/database.php
@@ -5,6 +5,6 @@ return [
 	'database_name' => 'charm_pj',
 	'server'        => 'localhost',
 	'username'      => 'root',
-	'password'      => 'root-2017',
+	'password'      => '1q2w3e4r',
 	'charset'       => 'utf8',
 ];
